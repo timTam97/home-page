b305a70469eb6e140af6d72e1e847b067d303a3a
Timothy Samraj <38150523+timTam97@users.noreply.github.com>
2023-12-18 19:05:38 +1100

    Add theming support (#21)
    * Add theming
    
    * Remove console logs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Head from "next/head";
 
-import { getPageTitle } from "notion-utils";
 import { NotionAPI } from "notion-client";
 import { NotionRenderer } from "react-notion-x";
 
@@ -18,12 +17,25 @@ export const getStaticProps = async (context) => {
 };
 
 export default function NotionPage({ recordMap }) {
+    const [dark, setDark] = React.useState<boolean>(true);
+    React.useEffect(function () {
+        const darkMode =
+            window?.matchMedia &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches;
+        window
+            .matchMedia("(prefers-color-scheme: dark)")
+            .addEventListener("change", (e) => setDark(e.matches));
+        setDark(darkMode);
+        return () => {
+            window
+                .matchMedia("(prefers-color-scheme: dark)")
+                .removeEventListener("change", () => {});
+        };
+    }, []);
+
     if (!recordMap) {
         return null;
     }
-
-    const title = getPageTitle(recordMap);
-    console.log(title, recordMap);
     return (
         <>
             <Head>
@@ -33,7 +45,7 @@ export default function NotionPage({ recordMap }) {
             <NotionRenderer
                 recordMap={recordMap}
                 fullPage={true}
-                darkMode={true}
+                darkMode={dark}
             />
         </>
     );
