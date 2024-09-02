09ad7620267e5cdf5391c0981d9ec2cf619b847f
Timothy Samraj <tsam0005@student.monash.edu>
2024-09-02 17:01:49 +1000

    add new page

---
+++ pages/hello/index.tsx
@@ -0,0 +1,190 @@
+import Image from "next/image";
+
+export default function Home() {
+    return (
+        <>
+            {/* <div className="-screen">
+        <div className="w-1/2 h-1/2 flex items-center justify-center text-center">
+           Among the dozens of Nike-branded items the site offers are the U.S. sportswear maker’s Phantom GT2 Elite soccer boots, for 29,999 roubles, or around $330.
+The man who got these shoes to Russia is Wijnand Herinckx, a 40-year-old Dutch citizen who lives in Moscow. Since the conflict began, Herinckx has built a thriving business that provides Russian consumers with Western goods whose makers have pulled out of Russia.
+“Nike does not want their products to be shipped to Russia,” Herinckx told Reuters in a video call from his office on the outskirts of Moscow, where shelves are stacked with boxes of Western branded footwear. But he added: “They are also not telling us not to do it.”
+Both Nike(NKE.N), opens new tab and Lego told Reuters they have not authorized Herinckx’s imports of their goods to Russia.<br></br><br></br>
+By examining customs data, corporate records and internal company documents, and by speaking to Herinckx himself, Reuters learned how his business obtains branded goods including Nike and Lego: It uses intermediaries with no apparent connection to Russia as buyers, then ships the goods to Russia – often via Turkey – and finally delivers them to retailers in Russia.
+There are at least dozens of firms like Herinckx’s employing grey-market methods to get Western goods to Russia, according to a Reuters analysis of customs data. His operation shows how attempts by Western governments and brands to isolate Russia’s economy are crashing into a reality of global business: Where there’s demand, someone will meet it.
+Western governments’ restrictions have mostly focused on industrial products that can be used to build weapons for Russia’s war machine. Such products are usually subject to U.S. and European Union sanctions. Herinckx said his focus is on consumer goods not covered by sanctions. Reuters found no evidence that his firm was violating sanctions.
+        </div></div> */}
+            {/* <article className="prose prose-stone dark:prose-invert lg:prose-xl flex flex-col justify-center items-center text-center h-screen"> */}
+            <div className="h-screen flex items-center justify-center text-center">
+                <article className="prose prose-stone dark:prose-invert lg:prose-xl px-6 py-10">
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <br />
+                    <h1>Garlic bread with cheese: What the science tells us</h1>
+                    <p>
+                        For years parents have espoused the health benefits of
+                        eating garlic bread with cheese to their children, with
+                        the food earning such an iconic status in our culture
+                        that kids will often dress up as warm, cheesy loaf for
+                        Halloween. For years parents have espoused the health
+                        benefits of eating garlic bread with cheese to their
+                        children, with the food earning such an iconic status in
+                        our culture that kids will often dress up as warm,
+                        cheesy loaf for Halloween. For years parents have
+                        espoused the health benefits of eating garlic bread with
+                        cheese to their children, with the food earning such an
+                        iconic status in our culture that kids will often dress
+                        up as warm, cheesy loaf for Halloween. For years parents
+                        have espoused the health benefits of eating garlic bread
+                        with cheese to their children, with the food earning
+                        such an iconic status in our culture that kids will
+                        often dress up as warm, cheesy loaf for Halloween. For
+                        years parents have espoused the health benefits of
+                        eating garlic bread with cheese to their children, with
+                        the food earning such an iconic status in our culture
+                        that kids will often dress up as warm, cheesy loaf for
+                        Halloween. For years parents have espoused the health
+                        benefits of eating garlic bread with cheese to their
+                        children, with the food earning such an iconic status in
+                        our culture that kids will often dress up as warm,
+                        cheesy loaf for Halloween. For years parents have
+                        espoused the health benefits of eating garlic bread with
+                        cheese to their children, with the food earning such an
+                        iconic status in our culture that kids will often dress
+                        up as warm, cheesy loaf for Halloween.
+                    </p>
+                    <p>
+                        But a recent study shows that the celebrated appetizer
+                        may be linked to a series of rabies cases springing up
+                        around the country.
+                    </p>
+                </article>
+            </div>
+
+            {/* <div className="flex h-screen">
+                <div className="m-auto">
+                    <p className="break-normal">
+                        Among the dozens of Nike-branded items the site offers<br/>
+                        are the U.S. sportswear maker’s Phantom GT2 Elite soccer
+                        boots, for 29,999 roubles, or around $330.
+                    </p>
+                </div>
+            </div> */}
+        </>
+    );
+    // return (
+    //   <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    //     <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    //       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    //         Get started by editing&nbsp;
+    //         <code className="font-mono font-bold">app/page.tsx</code>
+    //       </p>
+    //       <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+    //         <a
+    //           className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+    //           href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //           target="_blank"
+    //           rel="noopener noreferrer"
+    //         >
+    //           By{" "}
+    //           <Image
+    //             src="/vercel.svg"
+    //             alt="Vercel Logo"
+    //             className="dark:invert"
+    //             width={100}
+    //             height={24}
+    //             priority
+    //           />
+    //         </a>
+    //       </div>
+    //     </div>
+
+    //     <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
+    //       <Image
+    //         className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+    //         src="/next.svg"
+    //         alt="Next.js Logo"
+    //         width={180}
+    //         height={37}
+    //         priority
+    //       />
+    //     </div>
+
+    //     <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+    //       <a
+    //         href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <h2 className="mb-3 text-2xl font-semibold">
+    //           Docs{" "}
+    //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+    //             -&gt;
+    //           </span>
+    //         </h2>
+    //         <p className="m-0 max-w-[30ch] text-sm opacity-50">
+    //           Find in-depth information about Next.js features and API.
+    //         </p>
+    //       </a>
+
+    //       <a
+    //         href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <h2 className="mb-3 text-2xl font-semibold">
+    //           Learn{" "}
+    //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+    //             -&gt;
+    //           </span>
+    //         </h2>
+    //         <p className="m-0 max-w-[30ch] text-sm opacity-50">
+    //           Learn about Next.js in an interactive course with&nbsp;quizzes!
+    //         </p>
+    //       </a>
+
+    //       <a
+    //         href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <h2 className="mb-3 text-2xl font-semibold">
+    //           Templates{" "}
+    //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+    //             -&gt;
+    //           </span>
+    //         </h2>
+    //         <p className="m-0 max-w-[30ch] text-sm opacity-50">
+    //           Explore starter templates for Next.js.
+    //         </p>
+    //       </a>
+
+    //       <a
+    //         href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    //         className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         <h2 className="mb-3 text-2xl font-semibold">
+    //           Deploy{" "}
+    //           <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+    //             -&gt;
+    //           </span>
+    //         </h2>
+    //         <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
+    //           Instantly deploy your Next.js site to a shareable URL with Vercel.
+    //         </p>
+    //       </a>
+    //     </div>
+    //   </main>
+    // );
+}
