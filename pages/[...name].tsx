b28859191cd86e07f86e891ee47c4058715a5bb3
Timothy Samraj <tssamraj@amazon.com>
2024-09-16 12:19:40 +1000

    Add frontmatter

--- pages/[...name].tsx
+++ pages/[...name].tsx
@@ -10,7 +10,9 @@ export const getServerSideProps = async (props: {
     return res.status === 200
         ? {
               props: {
-                  source: await serialize(await res.text()),
+                  source: await serialize(await res.text(), {
+                      parseFrontmatter: true,
+                  }),
               },
           }
         : {
@@ -21,6 +23,7 @@ export const getServerSideProps = async (props: {
 export default function Home({ source }) {
     return (
         <>
+            <title>{source.frontmatter.title}</title>
             <div className="h-max flex justify-center text-left pt-12 pb-8">
                 <article className="prose dark:prose-invert md:prose-lg lg:prose-xl px-8 w-0 mx-auto mt-auto grow">
                     <MDXRemote {...source} />
